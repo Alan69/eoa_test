fdb316b2b4f91fd827be203deef67e52ddb2881c
Alan69 <gameg1676@gmail.com>
2024-08-07 21:37:44 +0500

    bug fix

--- test_logic/serializers.py
+++ test_logic/serializers.py
@@ -11,14 +11,16 @@ class TestSerializer(serializers.ModelSerializer):
         model = Test
         fields = '__all__'
 
-class QuestionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Question
-        fields = '__all__'
-
 class OptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Option
+        fields = '__all__'
+
+class QuestionSerializer(serializers.ModelSerializer):
+    options = OptionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Question
         fields = '__all__'
 
 class ResultSerializer(serializers.ModelSerializer):
