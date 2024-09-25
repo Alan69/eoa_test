0cda235d54683809426a7de3f09186b9cc8e2419
Alan69 <gameg1676@gmail.com>
2024-09-25 12:58:32 +0500

    fix

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,18 @@
 from rest_framework import serializers
 from .models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+        token['email'] = user.email
+        token['first_name'] = user.first_name
+        token['last_name'] = user.last_name
+
+        return token
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
