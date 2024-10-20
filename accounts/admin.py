f1095c7f3130e8b2a8f6bf51c57f70256b951842
Alan69 <gameg1676@gmail.com>
2024-10-20 17:17:42 +0500

    fix

--- accounts/admin.py
+++ accounts/admin.py
@@ -11,7 +11,7 @@ class UserAdmin(BaseUserAdmin):
     list_filter = ('is_student', 'is_teacher', 'is_principal', 'is_staff', 'is_active', 'is_superuser', 'region')
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'region', 'email', 'phone_number', 'school', 'balance', 'payment_id')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'region', 'email', 'phone_number', 'school', 'balance', 'payment_id', 'test_is_started')}),
         ('Permissions', {'fields': ('is_active', 'is_student', 'is_teacher', 'is_principal', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
     )
     add_fieldsets = (
